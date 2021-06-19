a7072ed976348fef48c03baed8360f61f1de1749
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-19 13:20:41 +0000

    3th commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-   resources :books
-    get 'about' => 'homes#about'
+  resources :books
+  get 'about' => 'homes#about'
+  resources :users
+
 end
